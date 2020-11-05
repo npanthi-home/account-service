1f045077021b9ea45baf8ee96a506ae3e53c5d32
npanthi <nitinpanthi90@gmail.com>
2020-11-05 11:41:53 +0530

    initial commit.

---
+++ src/test/java/com/intuit/craft/account/AccountService/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intuit.craft.account.AccountService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
